b9e351cf0e67f0bd31d0e84349fc0775c94f1c5c
furihata <sanko.shoko@gmail.com>
2017-07-31 21:25:13 +0900

    update param

--- sample/sp/randomforest/main.cpp
+++ sample/sp/randomforest/main.cpp
@@ -79,8 +79,8 @@ void classification(const char *path) {
 
 	SP_ASSERT(loadMNIST(trainImages, trainLabels, testImages, testLabels, path));
 
-	RandomForestCls<10> rf(20, 100);
-	//RandomForestCls<10> rf(20, 1000); // slow
+	RandomForestCls<10> rf(15, 200);
+	//RandomForestCls<10> rf(15, 1000); // slow
 
 	const int treeNum = 80;
 	for (int i = 0; i < treeNum; i++) {
