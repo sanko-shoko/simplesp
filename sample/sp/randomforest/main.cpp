9ebeb23efea601c991fc1be3010823f660aaacc4
furihata <sanko.shoko@gmail.com>
2017-08-05 22:57:46 +0900

    refactoring

--- sample/sp/randomforest/main.cpp
+++ sample/sp/randomforest/main.cpp
@@ -79,8 +79,8 @@ void classification(const char *path) {
 
 	SP_ASSERT(loadMNIST(trainImages, trainLabels, testImages, testLabels, path));
 
-	RandomForestCls<10> rf(15, 200);
-	//RandomForestCls<10> rf(15, 1000); // slow
+	RandomForestCls rf(10, 15, 200);
+	//RandomForestCls rf(10, 15, 1000); // slow
 
 	const int treeNum = 80;
 	for (int i = 0; i < treeNum; i++) {
