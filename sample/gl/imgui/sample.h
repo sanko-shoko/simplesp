33cfa0c49b1dceecfd9d52a023ace0084fb564ec
furihata <sanko.shoko@gmail.com>
2017-11-04 21:18:22 +0900

    refactoring

--- sample/gl/imgui/sample.h
+++ sample/gl/imgui/sample.h
@@ -77,8 +77,6 @@ private:
 		bool show_test_window = true;
 		bool show_another_window = false;
 		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-		// 1. Show a simple window.
-		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug".
 		{
 			static float f = 0.0f;
 			ImGui::Text("Hello, world!");
