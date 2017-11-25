1c95d7550d48d4c986a5bb1fa4ab6b8001a3be2f
furihata <sanko.shoko@gmail.com>
2017-11-25 15:27:59 +0900

    update

--- sample/_devel/testcore/main.cpp
+++ sample/_devel/testcore/main.cpp
@@ -5,5 +5,21 @@ using namespace sp;
 int main(){ 
 	SP_PRINTF("compile test\n");
 
+	printf("%s", getCrntDir().c_str());
+	return 0;
+	XML xml;
+
+	xml.begin("test");
+	xml.add("val0", "aaa");
+	xml.add("val1", "bbb");
+	{
+		xml.begin("0");
+		xml.add("val00", "0");
+		xml.add("val01", "1");
+		xml.end();
+	}
+	xml.end();
+
+	xml.save("test.xml");
 	return 0;
 }
