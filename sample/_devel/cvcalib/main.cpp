a696c79ff3593a14580394f3a5d1ca1f8a8e1e2d
furihata <sanko.shoko@gmail.com>
2018-03-21 12:12:17 +0900

    add devel code

---
+++ sample/_devel/cvcalib/main.cpp
@@ -0,0 +1,72 @@
+﻿#include <vector>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    const Size bsize(5, 5);
+    const double bdistance = 30.0;
+
+    vector<Point3f> grid;
+    for (int y = 0; y < bsize.height; y++) {
+        for (int x = 0; x < bsize.width; x++) {
+            grid.push_back(Point3f(x, y, 0.0f) * bdistance);
+        }
+    }
+
+    VideoCapture cap(0);
+    if (cap.isOpened() == false) {
+        printf("could not find USB camera\n");
+        exit(0);
+    }
+
+    printf("'a' key : add detected points for calibration\n");
+    printf("'c' key : execute calibration (i >= 3) \n");
+    printf("'ESC' key : exit\n");
+
+    int key = 0;
+
+    // 27 = 'ESC' key
+    while ((key = waitKey(1)) != 27) {
+
+        // capture
+        Mat img;
+        cap >> img;
+
+        vector<Point2f> pointbuf;
+        const bool found = findCirclesGrid(img, bsize, pointbuf);
+
+        if (found == true) {
+            drawChessboardCorners(img, bsize, pointbuf, found);
+
+            static int cnt = 0;
+            static vector<vector<Point2f> > pixsList;
+            static vector<vector<Point3f> > objsList;
+
+            if (key == 'a') {
+                printf("add detected points (i = %d)\n", cnt++);
+                pixsList.push_back(pointbuf);
+                objsList.push_back(grid);
+
+            }
+
+            if (key == 'c' && cnt >= 3) {
+                Mat camMat, dist;
+                vector<Mat> rvecs, tvecs;
+
+                const double rms = calibrateCamera(objsList, pixsList, img.size(), camMat, dist, rvecs, tvecs);
+                printf("opencv rms error: %g\n", rms);
+
+                cout << "camMat = " << endl << camMat << endl;
+                cout << "dist = " << endl << dist << endl;
+            }
+        }
+
+        imshow("calibration", img);
+    }
+
+    destroyAllWindows();
+    return 0;
+}
+
--- sample/_devel/cvmat/main.cpp
+++ sample/_devel/cvmat/main.cpp
@@ -1,8 +1,14 @@
-﻿#include <opencv2/opencv.hpp>
+﻿#include <vector>
+#include <string>
+#include <fstream>
+#include <opencv2/opencv.hpp>
 
 #define SP_DATA_DIR "../../../../data"
 
+using namespace std;
 using namespace cv;
+
+bool loadCSV(Mat &mat, const char *path, int type);
 
 int main(){
 
@@ -63,6 +69,63 @@ int main(){
         std::cout << mat3 << std::endl;
 
     }
+    {
+        Mat mat0 = Mat::zeros(2, 2, CV_8UC1);
+        mat0.at<unsigned char>(0, 0) = 100;
+        mat0.at<unsigned char>(1, 1) = 100;
+
+        Mat mat1;
+        mat0.convertTo(mat1, CV_32FC1, 1.0 / 255.0);
+
+        cout << mat0 << endl;
+        cout << mat1 << endl;
+    }
+    {
+        Mat mat0 = Mat::eye(3, 3, CV_32FC1);
+
+        ofstream ofs("mat.csv");
+        ofs << format(mat0, Formatter::FMT_CSV) << endl;
+
+        Mat mat1;
+        loadCSV(mat1, "mat.csv", CV_32FC1);
+        cout << mat1 << endl;
+    }
     return 0;
 }
 
+bool loadCSV(Mat &mat, const char *path, int type) {
+    ifstream ifs(path);
+    if (ifs.fail()) return false;
+
+    vector<vector<double> > data;
+    
+    string str;
+    while (getline(ifs, str)){
+        vector<double> tmp;
+    
+        stringstream ss{ str };
+        string buf;
+        while (getline(ss, buf, ',')) {
+            double val = 0.0;
+            sscanf(buf.c_str(), "%lf", &val);
+
+            tmp.push_back(val);
+        }
+
+        if (tmp.size() > 0) {
+            data.push_back(tmp);
+        }
+    }
+    if (data.size() == 0) return false;
+
+    Mat mat64 = Mat::zeros(data.size(), data[0].size(), CV_64FC1);
+    for (int r = 0; r < data.size(); r++) {
+        for (int c = 0; c < data[r].size(); c++) {
+            mat64.at<double>(r, c) = data[r][c];
+        }
+    }
+
+    mat64.convertTo(mat, type);
+
+    return true;
+}
