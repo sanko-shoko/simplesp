95e5e8ffc93463db70506826b0c5e3e3fa057f2f
furihata <sanko.shoko@gmail.com>
2018-05-29 21:50:49 +0900

    update bp

--- sample/sp/beliefpropagation/main.cpp
+++ sample/sp/beliefpropagation/main.cpp
@@ -5,47 +5,61 @@ using namespace sp;
 int main(){
 
     //--------------------------------------------------------------------------------
-    // graph cut
+    // belief propagation
     //--------------------------------------------------------------------------------
 
     Mem2<Byte> img;
     SP_ASSERT(loadBMP(img, SP_DATA_DIR  "/image/Lenna.bmp"));
 
-    binalizeAdapt(img, img);
+    const int labelMax = 5;
+    const int step = SP_BYTEMAX / labelMax;
+
     for (int i = 0; i < img.size(); i++){
-        if (sp::rand() % 10 == 0){
-            img[i] = 255 - img[i];
-        }
+        img[i] = maxVal(0, minVal(SP_BYTEMAX, img[i] + ::rand() % 101 - 50));
     }
     saveBMP(img, "input.bmp");
 
+    {
+        Mem2<Byte> bin = img;
+        for (int i = 0; i < bin.size(); i++) {
+            bin[i] = ((bin[i] + step / 2) / step) * step;
+        }
+        saveBMP(bin, "bin.bmp");
+    }
 
-    const int kappa = 80;
+    {
+        const int kappa = 100;
 
-    GraphCut gc(img.size(), img.size() * 4);
+        BeliefPropagation bp(labelMax, img.size(), img.size() * 2);
 
-    for (int v = 0; v < img.dsize[1]; v++){
-        for (int u = 0; u < img.dsize[0]; u++){
-            gc.setNode(acsid2(img.dsize, u, v), img(u, v),  255 - img(u, v));
+        Mem3<int> costMap(labelMax, img.dsize[0], img.dsize[1]);
 
-            const int link[][2] = { { -1, 0 }, { 0, -1 }, { -1, -1 }, { +1, -1 } };
-            for (int d = 0; d < 4; d++){
-                const int x = u + link[d][0];
-                const int y = v + link[d][1];
+        for (int v = 0; v < img.dsize[1]; v++) {
+            for (int u = 0; u < img.dsize[0]; u++) {
+                for (int i = 0; i < labelMax; i++) {
+                    costMap(i, u, v) = ::abs(img(u, v) - i * step);
+                }
+                bp.setNode(acsid2(img.dsize, u, v), &costMap(0, u, v));
 
-                if (isInRect2(img.dsize, x, y) == true){
-                    gc.setLink(acsid2(img.dsize, u, v), acsid2(img.dsize, x, y), kappa);
+                const int link[][2] = { { -1, 0 }, { 0, -1 } };
+                for (int d = 0; d < 4; d++) {
+                    const int x = u + link[d][0];
+                    const int y = v + link[d][1];
+
+                    if (isInRect2(img.dsize, x, y) == true) {
+                        bp.setLink(acsid2(img.dsize, u, v), acsid2(img.dsize, x, y), kappa);
+                    }
                 }
             }
         }
-    }
 
-    gc.execute();
+        bp.execute(50);
 
-    for (int i = 0; i < img.size(); i++){
-        img[i] = (gc.getLabel(i) > 0) ? 0 : 255;
+        for (int i = 0; i < img.size(); i++) {
+            img[i] = bp.getLabel(i) * step;
+        }
+        saveBMP(img, "bp.bmp");
     }
-    saveBMP(img, "graphcut.bmp");
 
     return 0;
 }
