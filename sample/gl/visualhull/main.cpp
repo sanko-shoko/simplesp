a5a15bef27285e7b5809a3f758daf7d8b9e5a19d
furihata <sanko.shoko@gmail.com>
2018-07-22 17:24:06 +0900

    refactoring

--- sample/gl/visualhull/main.cpp
+++ sample/gl/visualhull/main.cpp
@@ -105,8 +105,6 @@ private:
     }
 
     virtual void display() {
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // render model
         {
