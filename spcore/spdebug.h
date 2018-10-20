7c9eea96aa896eb30969664027628f7e597bcd08
sanko-shoko <sanko.shoko@gmail.com>
2018-10-21 00:59:02 +0900

    refactoring

--- spcore/spdebug.h
+++ spcore/spdebug.h
@@ -60,69 +60,130 @@ namespace sp {
     //--------------------------------------------------------------------------------
     // print 
     //--------------------------------------------------------------------------------
-   
-    SP_CPUFUNC void print(const Cmp &cmp) {
-        SP_PRINTF("Cmp [ %+.5lf, %+.5lf ]\n", cmp.re, cmp.im);
+  
+    SP_CPUFUNC void _print(const int val) {
+        SP_PRINTF("%d", val);
     }
 
-    SP_CPUFUNC void print(const int dim, const int *dsize) {
-        SP_PRINTF("dim %d, ", dim);
+    SP_CPUFUNC void _print(const double val) {
+        SP_PRINTF("%g", val);
+    }
 
-        SP_PRINTF("dsize [ ");
-        for (int d = 0; d < dim; d++){
+    SP_CPUFUNC void _print(const Cmp &cmp) {
+        SP_PRINTF("[ %+.5lf, %+.5lf ]", cmp.re, cmp.im);
+    }
+    SP_CPUFUNC void print(const Cmp &cmp) {
+        SP_PRINTF("Cmp ");
+        _print(cmp);
+        SP_PRINTF("\n");
+    }
+
+    SP_CPUFUNC void _print(const int dim, const int *dsize) {
+        SP_PRINTF("[ ");
+        for (int d = 0; d < dim; d++) {
             SP_PRINTF("%d ", dsize[d]);
         }
-        SP_PRINTF("]\n");
+        SP_PRINTF("]");
+    }
+    SP_CPUFUNC void print(const int dim, const int *dsize) {
+        SP_PRINTF("dsize ");
+        _print(dim, dsize);
+        SP_PRINTF("\n");
     }
 
+    SP_CPUFUNC void _print(const Rect &rect) {
+        SP_PRINTF("[ ");
+        _print(rect.dim, rect.dbase);
+        SP_PRINTF(", ");
+        _print(rect.dim, rect.dsize);
+        SP_PRINTF(" ]");
+    }
     SP_CPUFUNC void print(const Rect &rect) {
-        SP_PRINTF("Rect dim %d, ", rect.dim);
-
-        SP_PRINTF("dbase [ ");
-        for (int i = 0; i < rect.dim; i++) {
-            SP_PRINTF("%d ", rect.dbase[i]);
-        }
-        SP_PRINTF("], ");
-
-        SP_PRINTF("dsize [ ");
-        for (int i = 0; i < rect.dim; i++) {
-            SP_PRINTF("%d ", rect.dsize[i]);
-        }
-        SP_PRINTF("]\n");
+        SP_PRINTF("Rect ");
+        _print(rect);
+        SP_PRINTF("\n");
     }
 
+    SP_CPUFUNC void _print(const Vec2 &vec) {
+        SP_PRINTF("[ %+.5lf %+.5lf ]", vec.x, vec.y);
+    }
     SP_CPUFUNC void print(const Vec2 &vec) {
-        SP_PRINTF("Vec2 [ %+.5lf %+.5lf ]\n", vec.x, vec.y);
+        SP_PRINTF("Vec2 ");
+        _print(vec);
+        SP_PRINTF("\n");
     }
 
+    SP_CPUFUNC void _print(const Vec3 &vec) {
+        SP_PRINTF("[ %+.5lf %+.5lf %+.5lf ]", vec.x, vec.y, vec.z);
+    }
     SP_CPUFUNC void print(const Vec3 &vec) {
-        SP_PRINTF("Vec3 [ %+.5lf %+.5lf %+.5lf ]\n", vec.x, vec.y, vec.z);
+        SP_PRINTF("Vec3 ");
+        _print(vec);
+        SP_PRINTF("\n");
     }
 
+    SP_CPUFUNC void _print(const Col3 &col) {
+        SP_PRINTF("[ %d %d %d ]", col.r, col.g, col.b);
+    }
     SP_CPUFUNC void print(const Col3 &col) {
-        SP_PRINTF("Col3 [ %d %d %d ]\n", col.r, col.g, col.b);
+        SP_PRINTF("Col3 ");
+        _print(col);
+        SP_PRINTF("\n");
     }
 
+    SP_CPUFUNC void _print(const VecPN2 &vec) {
+        SP_PRINTF("[ ");
+        _print(vec.pos);
+        SP_PRINTF(", ");
+        _print(vec.nrm);
+        SP_PRINTF(" ]");
+    }
     SP_CPUFUNC void print(const VecPN2 &vec) {
-        SP_PRINTF("VecPN2 [\n");
-        print(vec.pos);
-        print(vec.nrm);
-        SP_PRINTF("]\n");
+        SP_PRINTF("VecPN2 ");
+        _print(vec);
+        SP_PRINTF("\n");
     }
 
+    SP_CPUFUNC void _print(const VecPN3 &vec) {
+        SP_PRINTF("[ ");
+        _print(vec.pos);
+        SP_PRINTF(", ");
+        _print(vec.nrm);
+        SP_PRINTF(" ]");
+    }
     SP_CPUFUNC void print(const VecPN3 &vec) {
-        SP_PRINTF("VecPN3 [\n");
-        print(vec.pos);
-        print(vec.nrm);
-        SP_PRINTF("]\n");
+        SP_PRINTF("VecPN3 ");
+        _print(vec);
+        SP_PRINTF("\n");
     }
 
+    SP_CPUFUNC void _print(const Mesh2 &mesh) {
+        SP_PRINTF("[ ");
+        _print(mesh.pos[0]);
+        SP_PRINTF(", ");
+        _print(mesh.pos[1]);
+        SP_PRINTF(", ");
+        _print(mesh.pos[2]);
+        SP_PRINTF(" ]");
+    }
+    SP_CPUFUNC void print(const Mesh2 &mesh) {
+        SP_PRINTF("Mesh2 ");
+        _print(mesh);
+        SP_PRINTF("\n");
+    }
+    SP_CPUFUNC void _print(const Mesh3 &mesh) {
+        SP_PRINTF("[ ");
+        _print(mesh.pos[0]);
+        SP_PRINTF(", ");
+        _print(mesh.pos[1]);
+        SP_PRINTF(", ");
+        _print(mesh.pos[2]);
+        SP_PRINTF(" ]");
+    }
     SP_CPUFUNC void print(const Mesh3 &mesh) {
-        SP_PRINTF("Mesh3 [\n");
-        print(mesh.pos[0]);
-        print(mesh.pos[1]);
-        print(mesh.pos[2]);
-        SP_PRINTF("]\n");
+        SP_PRINTF("Mesh3 ");
+        _print(mesh);
+        SP_PRINTF("\n");
     }
 
     SP_CPUFUNC void print(const double *mat, const int rows, const int cols) {
@@ -136,6 +197,43 @@ namespace sp {
         SP_PRINTF("]\n");
     }
 
+    SP_CPUFUNC void _print(const Rot &rot) {
+        SP_PRINTF("[ %+.5lf %+.5lf %+.5lf %+.5lf ]", rot.qx, rot.qy, rot.qz, rot.qw);
+    }
+    SP_CPUFUNC void print(const Rot &rot) {
+        SP_PRINTF("Rot ");
+        _print(rot);
+        SP_PRINTF("\n");
+    }
+
+    SP_CPUFUNC void _print(const Pose &pose) {
+        SP_PRINTF("[ ");
+        _print(pose.rot);
+        SP_PRINTF(", ");
+        _print(pose.trn);
+        SP_PRINTF(" ]");
+    }
+    SP_CPUFUNC void print(const Pose &pose) {
+        SP_PRINTF("Pose ");
+        _print(pose);
+        SP_PRINTF("]\n");
+    }
+
+    SP_CPUFUNC void _print(const CamParam &cam) {
+        SP_PRINTF("[\n");
+        SP_PRINTF("size %d %d\n", cam.dsize[0], cam.dsize[1]);
+        SP_PRINTF("fx %.5lf, fy %.5lf\n", cam.fx, cam.fy);
+        SP_PRINTF("cx %.5lf, cy %.5lf\n", cam.cx, cam.cy);
+        SP_PRINTF("k1 %+.5lf, k2 %+.5lf, k3 %+.5lf\n", cam.k1, cam.k2, cam.k3);
+        SP_PRINTF("p1 %+.5lf, p2 %+.5lf\n", cam.p1, cam.p2);
+        SP_PRINTF("]");
+    }
+    SP_CPUFUNC void print(const CamParam &cam) {
+        SP_PRINTF("CamParam ");
+        _print(cam);
+        SP_PRINTF("\n");
+    }
+
     template <typename TYPE>
     SP_CPUFUNC void print(const ExPtr<TYPE> &mem) {
         SP_PRINTF("Mem ");
@@ -144,48 +242,27 @@ namespace sp {
 
         int size = 1;
         int unit[SP_DIMMAX] = { 0 };
-        for (int i = 0; i < mem.dim; i++){
+        for (int i = 0; i < mem.dim; i++) {
             size *= mem.dsize[i];
             unit[i] = size;
         }
 
-        for (int i = 0; i < size; i++){
-            for (int j = mem.dim - 1; j >= 0; j--){
-                if ((i + 0) % unit[j] == 0){
+        for (int i = 0; i < size; i++) {
+            for (int j = mem.dim - 1; j >= 0; j--) {
+                if ((i + 0) % unit[j] == 0) {
                     SP_PRINTF((j == 0) ? " [ " : "[\n");
                 }
             }
-            
-            const double val = mem.ptr[i];
-            SP_PRINTF("%g ", val);
 
-            for (int j = mem.dim - 1; j >= 0; j--){
-                if ((i + 1) % unit[j] == 0){
+            _print(mem.ptr[i]);
+            SP_PRINTF(" ");
+
+            for (int j = mem.dim - 1; j >= 0; j--) {
+                if ((i + 1) % unit[j] == 0) {
                     SP_PRINTF("]\n");
                 }
             }
         }
-    }
-
-    SP_CPUFUNC void print(const Rot &rot) {
-        SP_PRINTF("Rot [ %+.5lf %+.5lf %+.5lf %+.5lf ]\n", rot.qx, rot.qy, rot.qz, rot.qw);
-    }
-
-    SP_CPUFUNC void print(const Pose &pose) {
-        SP_PRINTF("Pose [\n");
-        print(pose.rot);
-        print(pose.trn);
-        SP_PRINTF("]\n");
-    }
-
-    SP_CPUFUNC void print(const CamParam &cam) {
-        SP_PRINTF("CamParam [\n");
-        SP_PRINTF("size %d %d\n", cam.dsize[0], cam.dsize[1]);
-        SP_PRINTF("fx %.5lf, fy %.5lf\n", cam.fx, cam.fy);
-        SP_PRINTF("cx %.5lf, cy %.5lf\n", cam.cx, cam.cy);
-        SP_PRINTF("k1 %+.5lf, k2 %+.5lf, k3 %+.5lf\n", cam.k1, cam.k2, cam.k3);
-        SP_PRINTF("p1 %+.5lf, p2 %+.5lf\n", cam.p1, cam.p2);
-        SP_PRINTF("]\n");
     }
 
     template<typename TYPE>
@@ -193,7 +270,6 @@ namespace sp {
         SP_PRINTF("%s ", name);
         print(data);
     }
-
 
     //--------------------------------------------------------------------------------
     // util
