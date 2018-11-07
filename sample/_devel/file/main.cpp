5efe8bebef87bfef7b009fc517f8165c92736833
sanko-shoko <sanko.shoko@gmail.com>
2018-11-07 22:49:23 +0900

    refactoring

--- sample/_devel/file/main.cpp
+++ sample/_devel/file/main.cpp
@@ -5,29 +5,47 @@ using namespace sp;
 
 int main(){ 
 
-    CamParam cam = getCamParam(640, 480);
-    saveText("cam.txt", cam);
-    cam = getCamParam(320, 240);
-    loadText("cam.txt", cam);
-    print(cam);
+    {
+        CamParam cam = getCamParam(640, 480);
+        saveText("cam.txt", cam);
+        cam = getCamParam(320, 240);
+        loadText("cam.txt", cam);
+        print(cam);
 
-    Pose pose = zeroPose();
-    saveText("pose.txt", pose);
-    pose = getPose(getRot(1, 1, 1, 1));
-    loadText("pose.txt", pose);
-    print(pose);
+        Pose pose = zeroPose();
+        saveText("pose.txt", pose);
+        pose = getPose(getRot(1, 1, 1, 1));
+        loadText("pose.txt", pose);
+        print(pose);
 
-    Vec3 vec = getVec(0.0, 0.0, 0.0);
-    saveText("vec.txt", vec);
-    vec = getVec(2.0, 2.0, 3.0);
-    loadText("vec.txt", vec);
-    print(vec);
+        Vec3 vec = getVec(0.0, 0.0, 0.0);
+        saveText("vec.txt", vec);
+        vec = getVec(2.0, 2.0, 3.0);
+        loadText("vec.txt", vec);
+        print(vec);
 
-    Mat mat = zeroMat(3, 3);
-    saveText("mat.txt", mat);
-    mat = eyeMat(3, 3);
-    loadText("mat.txt", mat);
-    print(mat);
+        Mat mat = zeroMat(3, 3);
+        saveText("mat.txt", mat);
+        mat = eyeMat(3, 3);
+        loadText("mat.txt", mat);
+        print(mat);
+    }
 
+    {
+        XML xml;
+
+        xml.begin("test");
+        xml.add("val0", "aaa");
+        xml.add("val1", "bbb");
+        {
+            xml.begin("0");
+            xml.add("val00", "0");
+            xml.add("val01", "1");
+            xml.end();
+        }
+        xml.end();
+
+        xml.save("test.xml");
+    }
     return 0;
 }
